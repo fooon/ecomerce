33c3cb0cfe07e12192acbefa3b9206e2ddfe078b
fooon <foooon0@gmail.com>
2023-05-02 16:14:14 +0300

    fixed link for /goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { MainPage, CategoryPage, ProductPage, CartPage, GoodsPage, LoginPage, RegistrationPage } from "./pages";
-import { cartActions, getIsAuth, getUserLoadStatus } from './store';
+import { cartActions, getIsAuth, getUserLoadStatus, getUserLogin } from './store';
 import { Header, Footer } from "src/components";
 import { Layout, message } from 'antd';
 import { useSelector } from 'react-redux';
@@ -9,6 +9,7 @@ import { useAppDispatch } from "src/hooks/useAppDispatch";
 
 export const App = () => {
   const isAuth = useSelector(getIsAuth);
+  const login = useSelector(getUserLogin);
   const prevIsAuth = useRef<boolean>(isAuth);
   const dispatch = useAppDispatch();
 
@@ -33,7 +34,7 @@ export const App = () => {
             <Route path="/category/:ids" element={<CategoryPage/>}/>
             <Route path="/product/:ids" element={<ProductPage/>}></Route>
             <Route path="/cart" element={isAuth ? <CartPage/> : <Navigate to="/login"/>}/>
-            <Route path="/goods" element={<GoodsPage/>}></Route>
+            <Route path="/goods" element={login == 'admin' ? <GoodsPage/> : <Navigate to="/"/>}></Route>
             <Route path="/login" element={isAuth ? <Navigate to="/"/> : <LoginPage/>}></Route>
             <Route path="/registration" element={isAuth ? <Navigate to="/"/> : <RegistrationPage/>}></Route>
             <Route path="*" element={<Navigate to="/"/>}></Route>
